d5d2557daf51dc975d77ca5b6f2ac5f46ffeb705
Cevdet Gulser <c.gulser86@gmail.com>
2022-04-26 18:35:21 +0200

    26.04.2022

--- src/C1.java
+++
@@ -1,7 +0,0 @@
-public class C1 {
-    public static void main(String[] args) {
-        // GitHub deneme repo
-        System.out.println("version branch olustu");
-        System.out.println("Hello World");
-    }
-}
